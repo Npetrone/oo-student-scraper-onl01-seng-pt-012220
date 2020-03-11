fbfa424907561515b0258e783160e0a3dd09b979
Nicholas Petrone <nickpetrone92@gmail.com>
2020-03-11 03:32:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -42,13 +42,13 @@ class Scraper
       student[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
     end
    
-    # student_sm = {:twitter => twitter,
-    # :linkedin => linkedin,
-    # :github => github,
-    # :blog => blog,
-    # :profile_quote => profile_quote,
-    # :bio => bio}
-    # student << student_sm
+    student_sm = {:twitter => twitter,
+    :linkedin => linkedin,
+    :github => github,
+    :blog => blog,
+    :profile_quote => profile_quote,
+    :bio => bio}
+    student << student_sm
     # binding.pry
     student
   end
