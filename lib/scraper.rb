3f8fd82b7108c6133e245e80d1ec1e321b2f3b73
Nicholas Petrone <nickpetrone92@gmail.com>
2020-03-11 02:53:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
     students = {}
-    socialmedia = doc.css
+    socialmedia = doc.css("social-icon-container")
     
     
     
